b3e2d024cd9b66773afebaa646f05b33cedc1393
param-v <112659738+param-v@users.noreply.github.com>
2022-09-14 14:49:01 -0600

    Update main.js

--- main.js
+++ main.js
@@ -16,63 +16,22 @@ function btnClicked() {
   let sciPercent = +document.getElementById('sci-percent').value;
 
   // Calculate & Output Letter Grades
-  // English
-  let engLetter;
-  if (engPercent >= 80) {
-    engLetter = 'A';
-  } else if (engPercent >= 65) {
-    engLetter = 'B';
-  } else if (engPercent >= 55) {
-    engLetter = 'C';
-  } else if (engPercent >= 50) {
-    engLetter = 'D';
-  } else {
-    engLetter = 'F';
-  }
-  document.getElementById('eng-letter').innerHTML = engLetter;
+  document.getElementById("eng-letter").innerHTML = check(engPercent);
+  document.getElementById("soc-letter").innerHTML = check(socPercent);
+  document.getElementById("math-letter").innerHTML = check(mathPercent);
+  document.getElementById("sci-letter").innerHTML = check(sciPercent);
+}
 
-  // Social Studies
-  let socLetter;
-  if (socPercent >= 80) {
-    socLetter = 'A';
-  } else if (socPercent >= 65) {
-    socLetter = 'B';
-  } else if (socPercent >= 55) {
-    socLetter = 'C';
-  } else if (socPercent >= 50) {
-    socLetter = 'D';
+function check(percent) {
+  if (percent >= 80) {
+    return 'A';
+  } else if (percent >= 65) {
+    return 'B';
+  } else if (percent >= 55) {
+    return 'C';
+  } else if (percent >= 50) {
+    return 'D';
   } else {
-    socLetter = 'F';
+    return 'F';
   }
-  document.getElementById('soc-letter').innerHTML = socLetter;
-
-  // Mathematics
-  let mathLetter;
-  if (mathPercent >= 80) {
-    mathLetter = 'A';
-  } else if (mathPercent >= 65) {
-    mathLetter = 'B';
-  } else if (mathPercent >= 55) {
-    mathLetter = 'C';
-  } else if (mathPercent >= 50) {
-    mathLetter = 'D';
-  } else {
-    mathLetter = 'F';
-  }
-  document.getElementById('math-letter').innerHTML = mathLetter;
-
-  // Science
-  let sciLetter;
-  if (sciPercent >= 80) {
-    sciLetter = 'A';
-  } else if (sciPercent >= 65) {
-    sciLetter = 'B';
-  } else if (sciPercent >= 55) {
-    sciLetter = 'C';
-  } else if (sciPercent >= 50) {
-    sciLetter = 'D';
-  } else {
-    sciLetter = 'F';
-  }
-  document.getElementById('sci-letter').innerHTML = sciLetter;
 }
